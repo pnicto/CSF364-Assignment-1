92952657aee934df20df1d8f44089be530821686
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-03-18 10:38:53 +0530

    feat: very simple visualization

--- main.cpp
+++ main.cpp
@@ -6,12 +6,11 @@
  */
 #define RAYGUI_IMPLEMENTATION
 
+#include "jarvis_march.h"
 #include "raygui.h"
 #include "raylib.h"
 #include <iostream>
 #include <vector>
-
-#include "raylib.h"
 
 #if defined(PLATFORM_WEB)
 #include "emscripten/emscripten.h"
@@ -66,6 +65,16 @@ bool showConvexHull = false;
  *
  */
 const float toolbarHeight = 70;
+/**
+ * @brief A collection of points (x, y) to be used as test data
+ *
+ */
+std::vector<Vector2> dataPoints;
+/**
+ * @brief Represents the JarvisMarch object.
+ *
+ */
+JarvisMarch jm(dataPoints);
 
 //----------------------------------------------------------------------------------
 // Local Functions Declaration
@@ -87,6 +96,29 @@ int main()
     screenWidth = getDocumentBodyWidth();
     screenHeight = getDocumentBodyHeight();
 #endif
+
+    dataPoints.push_back({0, 3});
+    dataPoints.push_back({1.1, 1.2});
+    dataPoints.push_back({-2, 4});
+    dataPoints.push_back({-3, 12});
+    dataPoints.push_back({4, -16});
+    dataPoints.push_back({0, 0});
+    dataPoints.push_back({0, 10});
+    dataPoints.push_back({0, -10});
+
+    dataPoints.push_back({0, 20});
+    dataPoints.push_back({0, -20});
+
+    float centerX = screenWidth / 2.0f;
+    float centerY = (screenHeight + toolbarHeight) / 2.0f;
+    const float scale = 20.0f;
+    for (auto &point : dataPoints)
+    {
+        point.x = centerX + point.x * scale;
+        point.y = centerY + point.y * scale;
+    }
+
+    jm = JarvisMarch(dataPoints);
 
     InitWindow(screenWidth, screenHeight, "Convex Hull");
 
@@ -156,6 +188,20 @@ static void UpdateDrawFrame(void)
     {
     case JARVIS_MARCH: {
         GuiDrawText("Jarvis March Algorithm", {10, 10, 400, 50}, TEXT_ALIGN_LEFT, BLACK);
+        for (size_t i = 0; i < dataPoints.size(); i++)
+        {
+            DrawCircleV(dataPoints[i], 5, BLACK);
+        }
+
+        if (showConvexHull)
+        {
+
+            auto hull = jm.getConvexHull();
+            for (size_t i = 0; i < hull.size(); i++)
+            {
+                DrawLineV(hull[i], hull[(i + 1) % hull.size()], RED);
+            }
+        }
     }
     break;
     case KIRK_PATRICK_SEIDEL: {
