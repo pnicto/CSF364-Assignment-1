859f5a8d4a6e4a590100dd9971384243d7b8d8ec
333pranav <pranav03.sharma@gmail.com>
2024-03-25 21:42:23 +0530

    Check if the hull points are the same

--- test.sh
+++ test.sh
@@ -9,3 +9,24 @@ for ((i = 0; i < $((10**exponent)); i++)); do
     ( echo -n "("; shuf -i $xmin-$xmax -n 1 | tr -d '\n'; echo -n ","; shuf -i $ymin-$ymax -n 1 | tr -d '\n'; 
     echo ")" ) >> in.txt
 done
+hp1=pyout.txt
+hp2=jarvis.txt
+hp3=kps.txt
+flag=1
+if ! python3 test.py $hp1 $hp2 ; then
+    flag=0
+    echo "The Python and Jarvis March hull points are different"
+fi
+if ! python3 test.py $hp1 $hp3; then
+    flag=0
+    echo "The Python and Kirkpatrick-Seidel hull points are different"
+fi
+if ! python3 test.py $hp2 $hp3; then
+    flag=0
+    echo "The Jarvis March and Kirkpatrick-Seidel hull points are different"
+fi
+
+if [ $flag -eq 1 ]; then
+    echo "All hull points are the same"
+fi
+
