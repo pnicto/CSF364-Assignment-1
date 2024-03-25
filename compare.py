bb2b7f46023df98a1b4770e59b62285301bf6050
333pranav <pranav03.sharma@gmail.com>
2024-03-25 19:27:12 +0530

    Add script to check if the hull points from different algorithms are the same

---
+++ compare.py
@@ -0,0 +1,37 @@
+import sys
+from collections import defaultdict
+
+
+def readPoints(filename):
+    points = []
+    with open(filename, "r") as f:
+        for line in f:
+            coords = line.split(",")
+            x = coords[0][1:]
+            y = coords[1][:-2]
+            points.append((int(x), int(y)))
+    return points
+
+
+def compare(a, b):
+    if len(a) != len(b):
+        return False
+    d = defaultdict(int)
+    for p in a:
+        d[p] += 1
+
+    for p in b:
+        d[p] -= 1
+
+    for i in d:
+        if d[i] != 0:
+            return False
+    return True
+
+
+a = readPoints(sys.argv[1])
+b = readPoints(sys.argv[2])
+
+print(
+    "The hull points are the same" if compare(a, b) else "The hull points are different"
+)
