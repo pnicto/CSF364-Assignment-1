c472995abaf502e5e732677f97ac67cb8372e02e
333pranav <pranav03.sharma@gmail.com>
2024-03-25 18:47:21 +0530

    Write the hull points from the Python script to a file

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from scipy.spatial import ConvexHull
 points = []
-f = open("in.txt", "r")
-for x in f:
+inFile = open("in.txt", "r")
+for x in inFile:
     coords = x.split(',')
     points.append(((int(coords[0][1:])), int(coords[1][:-2])))
 
 hull = ConvexHull(points)
+outFile = open("pyout.txt", "w")
 for i in hull.vertices:
-    print(points[i])
+    print(f'({points[i][0]},{points[i][1]})')
+    outFile.write(f'({points[i][0]},{points[i][1]})\n')
