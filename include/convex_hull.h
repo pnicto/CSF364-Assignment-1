8d229c64d77561c51293294d2f998260d04d3a85
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-03-26 09:15:47 +0530

    docs: doxygen comments for the convex hull class,

--- include/convex_hull.h
+++ include/convex_hull.h
@@ -1,3 +1,8 @@
+/**
+ * @file convex_hull.h
+ * @brief Contains the declaration of the abstract class ConvexHullAlgorithm.
+ *
+ */
 #ifndef CONVEX_HULL_H
 #define CONVEX_HULL_H
 
@@ -10,22 +15,86 @@
 #include <utility>
 #include <vector>
 
+/**
+ * @brief Abstract class that represents a convex hull algorithm.
+ *
+ * This class is an abstract class that represents a convex hull algorithm. It contains the common methods and
+ * attributes that all convex hull algorithms should have.
+ */
 class ConvexHullAlgorithm
 {
   private:
+    /**
+     * @brief The collection of points to be used to compute the convex hull.
+     *
+     */
     std::vector<Vector2> points;
+    /**
+     * @brief Represents the convex hull.
+     *
+     */
     std::vector<Vector2> convexHull;
+    /**
+     * @brief Computes all the steps in the convex hull computation process.
+     *
+     */
     virtual void computeConvexHull() = 0;
+    /**
+     * @brief Represents the current step in the JarvisMarch object.
+     *
+     */
+    int currentStep;
 
   public:
+    /**
+     * @brief Destroy the ConvexHullAlgorithm object
+     *
+     */
     virtual ~ConvexHullAlgorithm() = default;
+    /**
+     * @brief Checks if the ConvexHullAlgorithm object has reached the last step.
+     *
+     * @return true If the ConvexHullAlgorithm object has reached the last step.
+     * @return false If the ConvexHullAlgorithm object has not reached the last step.
+     */
     virtual bool isFinished() = 0;
+    /**
+     * @brief Draws the current step of the ConvexHullAlgorithm object.
+     *
+     */
     virtual void draw() = 0;
+    /**
+     * @brief Gets the next step of the ConvexHullAlgorithm object.
+     *
+     */
     virtual void next() = 0;
+    /**
+     * @brief Gets the previous step of the ConvexHullAlgorithm object.
+     *
+     */
     virtual void previous() = 0;
+    /**
+     * @brief Get the number of steps in the convex hull computation process.
+     *
+     * @return int The number of steps.
+     */
     virtual int getNumberOfSteps() = 0;
+    /**
+     * @brief Gets the current step that is being drawn.
+     *
+     * @return int The current step.
+     */
     virtual int getCurrentStep() = 0;
+    /**
+     * @brief Sets the current step to the specified value.
+     *
+     * @param step The step to set.
+     */
     virtual void setCurrentStep(int stepIndex) = 0;
+    /**
+     * @brief Show the legend for the visualization of the ConvexHullAlgorithm computation.
+     *
+     */
     virtual void showLegend(bool *showLegend, Vector2 *windowPosition, Vector2 *windowSize, Vector2 *maxWindowSize,
                             Vector2 *contentSize, Vector2 *scroll, bool *moving, bool *resizing, bool *minimized,
                             float toolbarHeight, float bottomBarHeight, const char *title) = 0;
