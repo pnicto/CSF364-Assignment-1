0eab4a6a6f9e6f8006e3b7c4793ed1a88ccd0433
333pranav <pranav03.sharma@gmail.com>
2024-03-26 13:41:59 +0530

    Fix shell script to actually use correct paths and programs

--- test.sh
+++ test.sh
@@ -2,18 +2,18 @@
 
 checkPoints(){
     hp1=pyout.txt
-    hp2=jarvis.txt
-    hp3=kps.txt
+    hp2=jarvisout.txt
+    hp3=kpsout.txt
     flag=1
-    if ! python3 test.py $hp1 $hp2 ; then
+    if ! python3 compare.py $hp1 $hp2 ; then
         flag=0
         echo "The Python and Jarvis March hull points are different"
     fi
-    if ! python3 test.py $hp1 $hp3; then
+    if ! python3 compare.py $hp1 $hp3; then
         flag=0
         echo "The Python and Kirkpatrick-Seidel hull points are different"
     fi
-    if ! python3 test.py $hp2 $hp3; then
+    if ! python3 compare.py $hp2 $hp3; then
         flag=0
         echo "The Jarvis March and Kirkpatrick-Seidel hull points are different"
     fi
@@ -23,8 +23,8 @@ checkPoints(){
     fi
 }
 
-numPoints=500
-for i in $(seq 1 100) ; do
+numPoints=100000
+for i in $(seq 1 1) ; do
     python3 genPoints.py $numPoints
     checkPoints
 done
