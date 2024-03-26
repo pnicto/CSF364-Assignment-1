d2977b91b9c55affc3dc992a514de56427882ddb
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2024-03-26 19:08:53 +0530

    docs: add doxygen comments for the settings class

--- include/kirk_patrick_seidel.h
+++ include/kirk_patrick_seidel.h
@@ -140,18 +140,18 @@ class KirkpatrickSeidel : public ConvexHullAlgorithm
     /**
      * @brief Show the legend for the visualization of the KirkPatrickSeidel computation.
      *
-     * @param showLegend The variable used to indicate if the legend is to be displayed
-     * @param windowPosition The position of the legend window on the screen
-     * @param windowSize The size of the legend window
-     * @param maxWindowSize The size of the legend window in the maximized state
-     * @param contentSize The size of the content window inside the legend window
-     * @param scroll The object used to keep track of current position after scrolling
-     * @param moving The variable used to indicate if the floating window is moving
-     * @param resizing The variable used to indicate if the floating window is resizing
-     * @param minimized The variable used to indicate if the floating window is minimized
-     * @param toolbarHeight The height of the toolbar
-     * @param bottomBarHeight The height of the bottomBar
-     * @param title The title for the floating window
+     * @param showLegend The variable used to indicate if the legend is to be displayed.
+     * @param windowPosition The position of the legend window on the screen.
+     * @param windowSize The size of the legend window.
+     * @param maxWindowSize The size of the legend window in the maximized state.
+     * @param contentSize The size of the content window inside the legend window.
+     * @param scroll The object used to keep track of current position after scrolling.
+     * @param moving The variable used to indicate if the floating window is moving.
+     * @param resizing The variable used to indicate if the floating window is resizing.
+     * @param minimized The variable used to indicate if the floating window is minimized.
+     * @param toolbarHeight The height of the toolbar.
+     * @param bottomBarHeight The height of the bottomBar.
+     * @param title The title for the floating window.
      */
     void showLegend(bool *showLegend, Vector2 *windowPosition, Vector2 *windowSize, Vector2 *maxWindowSize,
                     Vector2 *contentSize, Vector2 *scroll, bool *moving, bool *resizing, bool *minimized,
